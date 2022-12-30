579f4586b2635184cce586350075fbc77c68bdda
thanh5.nguyen <thanh5.nguyen@VN-MF10-NC100VB.LGE.NET>
2022-12-30 15:16:22 +0700

    Add

--- src/main/java/reportConfig/AllureTestListener.java
+++ src/main/java/reportConfig/AllureTestListener.java
@@ -1,7 +1,5 @@
 package reportConfig;
 
-import java.io.File;
-
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -10,14 +8,9 @@ import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 import commonJquery.BaseTestJquery;
-import commonJquery.GlobalConstants;
 import io.qameta.allure.Attachment;
 
 public class AllureTestListener implements ITestListener {
-
-	private static String getTestMethodName(ITestResult iTestResult) {
-		return iTestResult.getMethod().getConstructorOrMethod().getName();
-	}
 
 	// Screenshot attachments for Allure
 	@Attachment(value = "Screenshot of {0}", type = "image/png")
@@ -25,26 +18,13 @@ public class AllureTestListener implements ITestListener {
 		return (byte[]) ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
 	}
 
-	// Text attachments for Allure
-//	@Attachment(value = "Text attachment of {0}", type = "text/plain")
-//	public static String saveTextLog(String message) {
-//		return message;
-//	}
-
-	// HTML attachments for Allure
-//	@Attachment(value = "{0}", type = "text/html")
-//	public static String attachHtml(String html) {
-//		return html;
-//	}
-
 	@Override
 	public void onTestFailure(ITestResult iTestResult) {
 		Object testClass = iTestResult.getInstance();
 		WebDriver driver = ((BaseTestJquery) testClass).getDriverInstance();
 		saveScreenshotPNG(iTestResult.getName(), driver);
-		//saveTextLog(getTestMethodName(iTestResult) + " failed and screenshot taken!");
 	}
-	
+
 	@Override
 	public void onStart(ITestContext iTestContext) {
 		// TODO Auto-generated method stub
@@ -55,6 +35,10 @@ public class AllureTestListener implements ITestListener {
 		// TODO Auto-generated method stub
 	}
 
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+		// TODO Auto-generated method stub
+	}
 
 	@Override
 	public void onFinish(ITestContext arg0) {
@@ -72,25 +56,4 @@ public class AllureTestListener implements ITestListener {
 		// TODO Auto-generated method stub
 	}
 
-	@Override
-	public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-//	public void deleteAllureReport() {
-//		try {
-//			String pathFolderDownload = GlobalConstants.PROJECT_PATH + "/allure-json";
-//			File file = new File(pathFolderDownload);
-//			File[] listOfFiles = file.listFiles();
-//			for (int i = 0; i < listOfFiles.length; i++) {
-//				if (listOfFiles[i].isFile()) {
-//					System.out.println(listOfFiles[i].getName());
-//					new File(listOfFiles[i].toString()).delete();
-//				}
-//			}
-//		} catch (Exception e) {
-//			System.out.print(e.getMessage());
-//		}
-//	}
 }
